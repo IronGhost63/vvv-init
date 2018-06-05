6e416c71dfe35a701d441930c6a0307e37213fcb
Jirayu Yingthawornsuk <me@jirayu.in.th>
2018-06-05 20:22:08 +0700

    move logic

--- src/index.php
+++ src/index.php
@@ -4,30 +4,36 @@ namespace WP63\Tools;
 require_once '../vendor/autoload.php';
 
 class VVVInit {
-  protected $write;
+  protected $terminal;
   protected $config;
   protected $config_path;
 
   function __construct( $argv ) {
     $this->config_path = $_SERVER['HOME'] . "/.vvv-init.json";
-    $this->write = new \League\CLImate\CLImate;
-    $this->loadConfig();
+    $this->terminal = new \League\CLImate\CLImate;
+    $this->loadConfigs();
+
+    if( isset( $argv[1] ) ) {
+      // Command is specified
+    } else {
+      // Command not specified
+      // Fallback to default behavior: init project
+      if( !$this->config->vvv_path ) {
+        echo PHP_EOL;
+        $this->terminal->backgroundRed('                               ');
+        $this->terminal->backgroundRed('  VVV path is not configured.  ');
+        $this->terminal->backgroundRed('                               ');
+        echo PHP_EOL;
+        $this->terminal->out('Use `vvv-init setPath path/to/vvv-custom.yml` to set VVV path');
+        $this->terminal->out('Or use `vvv-init reset` to reset configuration file.');
+        echo PHP_EOL;
+        exit;
+      }
+    }
   }
 
-  protected function loadConfig() {
+  protected function loadConfigs() {
     $config = json_decode( $this->config_path );
-
-    if( !$config ) {
-      echo PHP_EOL;
-      $this->write->backgroundRed('                               ');
-      $this->write->backgroundRed('  VVV path is not configured.  ');
-      $this->write->backgroundRed('                               ');
-      echo PHP_EOL;
-      $this->write->out('Use `vvv-init setPath path/to/vvv-custom.yml` to set VVV path');
-      $this->write->out('Or use `vvv-init reset` to reset configuration file.');
-      echo PHP_EOL;
-    }
-
     $this->config = $config;
   }
 
